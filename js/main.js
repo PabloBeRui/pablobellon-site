c7059df907e0106e49cc08c96894239850867372
PabloBeRui <121795105+PabloBeRui@users.noreply.github.com>
2025-04-29 17:41:21 +0200

    Add typewriter effect, update figure image, import temporary custom font, and wrap JS in DOMContentLoaded

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
-// background video 
+// Wait for the full HTML document to be loaded and parsed before executing scripts.
+// This ensures that all DOM elements exist when we try to manipulate them.
 
-const video = document.getElementById("bg-video");
+document.addEventListener("DOMContentLoaded", () => {
+  // background video
 
+  const video = document.getElementById("bg-video");
 
+  video.addEventListener("canplaythrough", () => {
+    video.playbackRate = 0.2;
+    // re-start
+    video.play();
+  });
 
-video.addEventListener("canplaythrough", () => {
-  
-  video.playbackRate = 0.2;
-  // re-start 
-  video.play();
+  /* Retro Terminal Typewriter Effect */
+
+  const text =
+    "I’m currently studying and working on new projects. Stay tuned for updates!";
+
+  const typewriterElement = document.getElementById("typewriter-text");
+
+  let textIndex = 0;
+
+  function typeWriter() {
+    if (textIndex < text.length) {
+      typewriterElement.innerHTML += text.charAt(textIndex);
+
+      textIndex++;
+
+      setTimeout(typeWriter, 60); //typing speed
+    } else {
+      typewriterElement.classList.add('typewriter-class-2');
+    }
+  }
+  typeWriter();
 });
