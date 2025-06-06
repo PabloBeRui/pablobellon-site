0b5388389474bd9c850eca31377f67066d585a2b
PabloBeRui <121795105+PabloBeRui@users.noreply.github.com>
2025-06-06 10:56:45 +0200

    Add My Progress data module and icons folder
    - Create js/data/my-progress-data.js containing the courses array
    - Add images/icons/my-progress-icons/ to hold course icon assets

---
+++ js/data/my-progress-data.js
@@ -0,0 +1,145 @@
+export const courses = [
+  {
+    id: "hackaboss-2023",
+    name: "HackABoss",
+    year: 2023,
+    totalHours: 400,
+    approved: false,
+    img: "../../images/icons/my-progress-icons/hackaboss.png",
+    url: "https://www.hackaboss.com/",
+    projects: [],
+    subjects: [
+      {
+        id: "web-full-stack-javascript",
+        name: "Web Full Stack Javascript",
+        approved: false,
+      },
+    ],
+  },
+  {
+    id: "campusdual-tic-2023",
+    name: "CampusDual TIC",
+    year: 2023,
+    totalHours: 0, // No se especificaron horas en el enunciado
+    approved: false,
+    img: "../../images/icons/my-progress-icons/campusdual.png",
+    url: "https://www.campusdual.com/",
+    projects: [],
+    subjects: [
+      {
+        id: "introduccion-a-la-programacion",
+        name: "Introducción a la Programación",
+        approved: false,
+      },
+    ],
+  },
+  {
+    id: "tokioschool-2024",
+    name: "TokioSchool",
+    year: 2024,
+    totalHours: 400,
+    approved: false,
+    img: "../../images/icons/my-progress-icons/tokioschool.png",
+    url: "https://www.tokioschool.com/",
+    projects: [],
+    subjects: [
+      {
+        id: "front-end-html5-css3-javascript",
+        name: "Front-End: HTML5, CSS3, JavaScript",
+        approved: false,
+      },
+    ],
+  },
+  {
+    id: "daw-2025",
+    name: "DAW",
+    year: 2025,
+    totalHours: 2000,
+    approved: false,
+    img: "../../images/icons/my-progress-icons/ilerna.png",
+    url: "https://www.ilerna.es/",
+    projects: [],
+    subjects: [
+      {
+        id: "lenguajes-de-marcas-y-sistemas-de-gestion-de-informacion",
+        name: "Lenguajes de marcas y sistemas de gestión de información",
+        approved: false,
+      },
+      {
+        id: "sistemas-informaticos",
+        name: "Sistemas informáticos",
+        approved: false,
+      },
+      {
+        id: "bases-de-datos",
+        name: "Bases de datos",
+        approved: false,
+      },
+      {
+        id: "programacion",
+        name: "Programación",
+        approved: false,
+      },
+      {
+        id: "entornos-de-desarrollo",
+        name: "Entornos de desarrollo",
+        approved: false,
+      },
+      {
+        id: "ingles-profesional",
+        name: "Inglés profesional",
+        approved: false,
+      },
+      {
+        id: "digitalizacion-aplicada-a-los-sectores-productivos",
+        name: "Digitalización aplicada a los sectores productivos",
+        approved: false,
+      },
+      {
+        id: "sostenibilidad-aplicada-al-sistema-productivo",
+        name: "Sostenibilidad aplicada al sistema productivo",
+        approved: false,
+      },
+      {
+        id: "diseno-de-interfaces-web",
+        name: "Diseño de interfaces web",
+        approved: false,
+      },
+      {
+        id: "itinerario-personal-para-la-empleabilidad-i",
+        name: "Itinerario personal para la empleabilidad I",
+        approved: false,
+      },
+      {
+        id: "desarrollo-web-entorno-cliente",
+        name: "Desarrollo web entorno cliente",
+        approved: false,
+      },
+      {
+        id: "desarrollo-web-entorno-servidor",
+        name: "Desarrollo web entorno servidor",
+        approved: false,
+      },
+      {
+        id: "despliegue-de-aplicaciones-web",
+        name: "Despliegue de aplicaciones web",
+        approved: false,
+      },
+      {
+        id: "itinerario-personal-para-la-empleabilidad-ii",
+        name: "Itinerario personal para la empleabilidad II",
+        approved: false,
+      },
+      {
+        id: "proyecto-intermodular-de-desarrollo-de-aplicaciones-web",
+        name: "Proyecto intermodular de desarrollo de aplicaciones web",
+        approved: false,
+      },
+      {
+        id: "modulo-profesional-optativo",
+        name: "Módulo profesional optativo",
+        approved: false,
+      },
+    ],
+  },
+];
