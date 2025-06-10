87fb0e98132c6694c4f38690d76e07f9604d0ba9
PabloBeRui <121795105+PabloBeRui@users.noreply.github.com>
2025-06-10 18:50:54 +0200

    feat(about-me): start about-me-render logic and update toggle styles

--- js/templates/about-me.js
+++ js/templates/about-me.js
@@ -19,32 +19,33 @@ const aboutMe = `<div id="about-me-div-content">
           </div>
 
           <div id="description-div">
-            <p>
-              He pasado la mayor parte de mi vida profesional trabajando en la
-              industria, pero siempre he sentido una gran pasión por la
-              informática y la tecnología. Hace un tiempo decidí dar un giro a
-              mi carrera y enfocarme en lo que realmente me apasiona: la
-              programación, especialmente el desarrollo
-              <span class="about-me-span">&nbspfront-end </span>.
-            </p>
-            <br />
+           <p>
+          I have spent most of my professional life working in industry, but I
+          have always felt a great passion for computing and technology. Some
+          time ago, I decided to shift my career and focus on what truly excites
+          me: programming, especially
+          <span class="about-me-span">&nbsp;front-end </span> development.
+        </p>
+        <br />
 
-            <p>
-              Para ello, he completado varios bootcamps intensivos como
-              <span class="about-me-span">&nbspHackABoss </span>,
-              <span class="about-me-span">&nbspTokioSchool </span>&nbsp;y
-              <span class="about-me-span">&nbspCampusDual </span>, donde aprendí
-              de forma práctica y rápida las herramientas y tecnologías más
-              utilizadas, como <span class="skills-span">&nbsp;HTML </span>,
-              <span class="skills-span">&nbsp;CSS </span>,
-              <span class="skills-span">&nbsp;JavaScript </span>,
-              <span class="skills-span">&nbsp;React </span> &nbsp; (<a
-                href="https://elelegido.netlify.app/"
-                >https://elelegido.netlify.app/</a
-              >) y <span class="skills-span">&nbsp;Git </span>. Actualmente,
-              estoy cursando el segundo año del ciclo de Desarrollo de
-              Aplicaciones Web (<span class="skills-span">&nbsp;DAW </span>).
-            </p>
+        <p>
+          To that end, I completed several intensive bootcamps such as
+          <span class="about-me-span">&nbsp;HackABoss </span>,
+          <span class="about-me-span">&nbsp;TokioSchool </span>, and
+          <span class="about-me-span">&nbsp;CampusDual </span>, where I learned,
+          in a practical and fast-paced way, the most widely used tools and
+          technologies like <span class="skills-span">&nbsp;HTML </span>,
+          <span class="skills-span">&nbsp;CSS </span>,
+          <span class="skills-span">&nbsp;JavaScript </span>,
+          <span class="skills-span">&nbsp;React </span> (<a
+            href="https://elelegido.netlify.app/"
+            >https://elelegido.netlify.app/</a
+          >), and <span class="skills-span">&nbsp;Git </span>. Currently, I am in
+          my second year of the Web Application Development program (<span
+            class="skills-span"
+            >&nbsp;DAW </span
+          >).
+        </p>
           </div>
         </div>
       </div>`;
