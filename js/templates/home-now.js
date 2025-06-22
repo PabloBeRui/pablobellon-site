f202ed873e060f6ae5dc909eef1e6dc796e7eb97
PabloBeRui <121795105+PabloBeRui@users.noreply.github.com>
2025-06-22 12:01:24 +0200

    feat(now-theme): updated styles, nav layout, and transition animation

--- js/templates/home-now.js
+++ js/templates/home-now.js
@@ -1,17 +1,15 @@
 const homeNow = ` 
 <nav id="change-style-button-nav" class="visible smooth-fade-in-upward">
+<button class="nav-button" id="about-me-button">About me</button>
+                <button class="nav-button" id="my-progress-button">My Progress</button>
           <button id="change-style-button" class="nav-button">80s</button>
         </nav><figure id="coming-soon-figure" class="smooth-fade-in-upward">
-            <img src="../../images/photo_transition.webp">
+            <img src="../../images/photo_transition.webp" id="img-transition-now">
             <div id="text-and-buttons-container">
               <figcaption
                 id="typewriter-text"
                 class="typewriter-class-2">I’m currently studying and working on new projects 👾 Stay tuned for updates!</figcaption>
-              <nav id="nav-buttons"  class="visible">
-                <button class="nav-button" id="about-me-button">About me</button>
-                <button class="nav-button" id="my-progress-button">My Progress</button>
-      
-              </nav>
+              
             </div>
           </figure>`;
 
